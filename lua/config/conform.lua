21a3d1613042433ee3b76407b53e403a2e39ce74
Mykhailo Muzyka <mykhailo.muzyka535@gmail.com>
2025-01-06 14:14:34 +0200

    Add gitignore-like pattern support for clang-format-
    Enable support for gitignore-like patterns in the clang-format-ignore file to exclude files from formatting.

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -9,10 +9,34 @@ require("conform").setup({
   }
 })
 
+local function glob_to_regex(pattern)
+  pattern = string.gsub(pattern, "%*", ".*")
+  return pattern
+end
+
+local function is_file_ignored(bufnr)
+  local file_path = vim.api.nvim_buf_get_name(bufnr)
+  local ignore_file = vim.fn.stdpath("config") .. "/clang-format-ignore"
+
+  if vim.fn.filereadable(ignore_file) == 1 then
+    local ignored_files = vim.fn.readfile(ignore_file)
+    for _, pattern in ipairs(ignored_files) do
+      local regex_pattern = glob_to_regex(pattern)
+      if string.match(file_path, regex_pattern) then
+        return true
+      end
+    end
+  end
+
+  return false
+end
+
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*",
     callback = function(args)
-      require("conform").format({ bufnr = args.buf })
+      if not is_file_ignored(args.buf) then
+        require("conform").format({ bufnr = args.buf })
+      end
     end,
 })
 
