dfff99fe7009697543e9e56c5ddb0945827a5f2f
Mykhailo Muzyka <mykhailo.muzyka535@gmail.com>
2025-01-30 13:43:56 +0200

    Fix error after p00f/clangd_extensio@db28f29be928d18cbfb86fbfb9f83f584f658feb

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -29,3 +29,37 @@ end, { silent = true, noremap = true, desc = 'toggle signature' })
 vim.api.nvim_set_keymap('n', 'gh', ':ClangdSwitchSourceHeader<CR>', default_opts)
 
 vim.api.nvim_set_keymap('n', '<leader>t', ':ToggleTerm<CR>', { silent = true, noremap = true, desc = 'Toggle terminal' })
+
+
+local function toggle_inlay_hints(buf, mode)
+    local group = vim.api.nvim_create_augroup("inlay_hints", { clear = true })
+
+    if mode == "always" then
+        vim.lsp.inlay_hint.enable(true)
+        vim.api.nvim_clear_autocmds({ group = group, buffer = buf })
+        vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, { group = group, buffer = buf, callback = function() vim.lsp.inlay_hint.enable(true) end })
+    elseif mode == "never" then
+        vim.lsp.inlay_hint.enable(false)
+        vim.api.nvim_clear_autocmds({ group = group, buffer = buf })
+    elseif mode == "default" then
+        vim.lsp.inlay_hint.enable(true)
+        vim.api.nvim_clear_autocmds({ group = group, buffer = buf })
+        vim.api.nvim_create_autocmd("InsertEnter", { group = group, buffer = buf, callback = function() vim.lsp.inlay_hint.enable(false) end })
+        vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, { group = group, buffer = buf, callback = function() vim.lsp.inlay_hint.enable(true) end })
+    end
+    vim.g.Clangd_inlay_mode = mode or "default"
+end
+
+vim.keymap.set("n", "<leader>li", function()
+    toggle_inlay_hints(buf, "default")
+end, { buffer = buf, desc = "[l]sp [i]nlay [h]ints toggle (insert mode)" })
+
+vim.keymap.set("n", "<leader>lh", function()
+    toggle_inlay_hints(buf, "never")
+end, { buffer = buf, desc = "[l]sp [h]ints toggle (never)" })
+
+vim.keymap.set("n", "<leader>la", function()
+    toggle_inlay_hints(buf, "always")
+end, { buffer = buf, desc = "[l]sp [a]lways show inlay [h]ints" })
+
+toggle_inlay_hints(buf, vim.g.Inlay_hints)
